08ffe30277bee40337917ec893b93a17c441808f
Geoffrey Moluba <geoffreymoluba@Geoffreys-MacBook-Pro.local>
2023-08-29 23:52:25 -0400

    initiating API with brightdata

--- app/activateScraper/route.ts
+++ app/activateScraper/route.ts
@@ -1,8 +1,20 @@
+import { NextApiRequest } from "next";
+
+type Body = {
+    search: string;
+}
+export async function GET(req: NextApiRequest){
+    const search = req.body.search;  
+}
+
+
 //THIS NEW NEXT JS13.2 METHODE WE CAN EXPORT THE FOLLOWING
 // export async function GET(request: Request) {}
 // export async function POST(request: Request) {}
 // export async function DELETE(request: Request) {}
 
-export async function POST(request: Request){
-    
-}
+// export async function GET(request: Request){
+//     return new Response('Hello, MLB Teams!', {
+//         status: 200,
+//       });
+// }
